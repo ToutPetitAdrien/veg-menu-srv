ef831fac500ab5c72c04c825959d2ca63b00a805
Adrien Mulattieri <mula.adri@gmail.com>
2020-12-26 08:46:51 +0100

    refactor: 💡 Recipe parser use Deno based lib

--- parsers/recipe.ts
+++ parsers/recipe.ts
@@ -1,6 +1,5 @@
 import * as R from "https://x.nest.land/ramda@0.27.0/source/index.js";
-import { parse } from "https://cdn.skypack.dev/node-html-parser?dts";
-import * as log from "https://deno.land/std@0.76.0/log/mod.ts";
+import * as log from "https://deno.land/std@0.74.0/log/mod.ts";
 
 import { Ingredient, Recipe } from "../types.ts";
 import { parseIngredient } from "./ingredient.ts";
@@ -12,54 +11,54 @@ const CREATED_AT_SELECTOR = "div.blog-main div.fancy_categories time";
 const PREPARATION_TIME_SELECTOR = "span.wpurp-recipe-prep-time";
 const COOKING_TIME_SELECTOR = "span.wpurp-recipe-cook-time";
 const SERVINGS_SELECTOR = "input.advanced-adjust-recipe-servings";
-const INGREDIENTS_SELECTOR = "div.wpurp-recipe-ingredients";
+const INGREDIENTS_SELECTOR = "wpurp-recipe-ingredients";
 const INGREDIENTS_GROUP_SELECTOR =
-  "div.wpurp-recipe-ingredient-group-container";
-const INGREDIENTS_ROW_SELECTOR = "div.wpurp-rows-row";
-const INGREDIENTS_LIST_SELECTOR = "ul.wpurp-recipe-ingredient-container";
-const INGREDIENTS_LIST_ITEM_SELECTOR = "li.wpurp-recipe-ingredient";
-const INSTRUCTIONS_ITEM_SELECTOR =
-  "ol.wpurp-recipe-instruction-container li.wpurp-recipe-instruction";
+  "wpurp-recipe-ingredient-group-container";
+const INGREDIENTS_ROW_SELECTOR = "wpurp-rows-row";
+const INGREDIENTS_LIST_SELECTOR = "wpurp-recipe-ingredient-container";
+const INGREDIENTS_LIST_ITEM_SELECTOR = "wpurp-recipe-ingredient";
+const INSTRUCTIONS_ITEM_SELECTOR = "wpurp-recipe-instruction";
 const INSTRUCTIONS_TEXT_SELECTOR = "span.wpurp-recipe-instruction-text";
 
-export const parseRecipe = (
-  html: string,
-): Omit<Recipe, "slug" | "createdAtTimestamp"> => {
-  const root = parse(html);
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
-  const title = root.querySelector(TITLE_SELECTOR).rawText;
-  const description = root.querySelector(DESCRIPTION_SELECTOR).rawText;
-  const photoUrl = root.querySelector(PHOTO_SELECTOR).attributes.src;
-  const createdAt = root.querySelector(CREATED_AT_SELECTOR).attributes.datetime;
-
-  const preparationTime =
-    R.head(root.querySelectorAll(PREPARATION_TIME_SELECTOR)).rawText;
-  const cookingTime =
-    R.head(root.querySelectorAll(COOKING_TIME_SELECTOR)).rawText;
-  const servings = root.querySelector(SERVINGS_SELECTOR).attributes.value;
+export const parseRecipe = async (html: string): Omit<Recipe, "slug" | "createdAtTimestamp"> => {
+  const root = new DOMParser().parseFromString(
+    html,
+    "text/html",
+  );
+  const title = root.querySelector(TITLE_SELECTOR)?.textContent;
+  const description = root.querySelector(DESCRIPTION_SELECTOR)?.textContent;
+  const photoUrl = root.querySelector(PHOTO_SELECTOR)?.getAttribute("src");
+  const createdAt = root.querySelector(CREATED_AT_SELECTOR)?.getAttribute("datetime");
+  
+  const preparationTime = R.head(root.querySelectorAll(PREPARATION_TIME_SELECTOR)).textContent;
+  const cookingTime = R.head(root.querySelectorAll(COOKING_TIME_SELECTOR)).textContent;
+  const servings = root.querySelector(SERVINGS_SELECTOR)?.getAttribute("value");
 
   let ingredients: Array<Ingredient> = [];
   let otherIngredients: Array<Ingredient> = [];
 
-  const ingredientsSelector = root.querySelectorAll(INGREDIENTS_SELECTOR)[1];
-  const ingredientsGroupSelector = ingredientsSelector.querySelectorAll(
+  const ingredientsSelector = root.getElementsByClassName(INGREDIENTS_SELECTOR)[1];
+  const ingredientsGroupSelector = ingredientsSelector.getElementsByClassName(
     INGREDIENTS_GROUP_SELECTOR,
   );
+
   for (const ingrdGroup of ingredientsGroupSelector) {
     const currentIngredients = [];
-    const [titleSelector, ingredientsSelector] = ingrdGroup.querySelectorAll(
+    const [titleSelector, ingredientsSelector] = ingrdGroup.getElementsByClassName(
       INGREDIENTS_ROW_SELECTOR,
     );
-    const title = titleSelector.rawText.trim();
+    const title = titleSelector.textContent.trim();
 
     const ingredientsListSelector = R.head(
-      ingredientsSelector.querySelectorAll(INGREDIENTS_LIST_SELECTOR),
+      ingredientsSelector.getElementsByClassName(INGREDIENTS_LIST_SELECTOR),
     );
     const ingredientsListItemsSelector = ingredientsListSelector
-      .querySelectorAll(INGREDIENTS_LIST_ITEM_SELECTOR);
+      .getElementsByClassName(INGREDIENTS_LIST_ITEM_SELECTOR);
 
     for (const ingrd of ingredientsListItemsSelector) {
-      const ingredient = ingrd.rawText;
+      const ingredient = ingrd.textContent;
       if (ingredient) {
         currentIngredients.push(parseIngredient(ingredient));
       }
@@ -75,22 +74,22 @@ export const parseRecipe = (
   }
 
   const instructions = [];
-  const instructionsItemSelector = root.querySelectorAll(
+  const instructionsItemSelector = root.getElementsByClassName(
     INSTRUCTIONS_ITEM_SELECTOR,
   );
   for (const instructionSelector of instructionsItemSelector) {
     const text =
-      instructionSelector.querySelector(INSTRUCTIONS_TEXT_SELECTOR).rawText;
+      instructionSelector.querySelector(INSTRUCTIONS_TEXT_SELECTOR)?.textContent;
     instructions.push(text);
   }
-
+  
   return {
     title,
     description,
     photoUrl,
     preparationTime,
     cookingTime,
-    servings: 2,
+    servings,
     ingredients,
     otherIngredients,
     instructions,
